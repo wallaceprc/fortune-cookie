55e528b90faf71b3622e1e0ef5c24601d75e5a3c
Wallace Rocha <wallacerochascom@gmail.com>
2022-05-12 14:09:43 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      textoFrase: '',
+      img: require('./src/images/biscoito.png')
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+    this.fechaBiscoito = this.fechaBiscoito.bind(this);
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.',
+      'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+  }
+
+  quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+
+    this.setState({
+      textoFrase: '"' + this.frases[numeroAleatorio] + '"',
+      img: require('./src/images/biscoitoAberto.png')
+    })
+  }
+
+  fechaBiscoito(){
+    this.setState({
+      textoFrase: '',
+      img: require('./src/images/biscoito.png')
+    })
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image
+          source={this.state.img}
+          style={styles.img} 
+        />
+        <Text style={styles.textoFrase} >{this.state.textoFrase}</Text>
+        <TouchableOpacity style={styles.botaoQuebraBiscoito} onPress={this.quebraBiscoito} >
+          <View style={styles.btnArea} >
+            <Text style={styles.btnTexto} >Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botaoFechaBiscoito} onPress={this.fechaBiscoito} >
+          <View style={styles.btnArea} >
+            <Text style={styles.btnTexto} >Fechar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  img: {
+    width: 250,
+    height: 250
+  },
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botaoQuebraBiscoito: {
+    width: 230,
+    height: 50,
+    borderWidth:2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  botaoFechaBiscoito: {
+    width: 230,
+    height: 50,
+    borderWidth:2,
+    borderColor: '#dd7b22',
+    borderRadius: 25,
+    marginTop: 10
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
+});
+
+export default App;
